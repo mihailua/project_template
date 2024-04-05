ffcfcb5d49baec9a11b7d14f75067274fa003e2d
Mykhailo Vakoliuk <mihaylgg@gmail.com>
2024-04-05 23:54:26 +0100

    IO functions docstrings added

--- app/io/input.py
+++ app/io/input.py
@@ -1,11 +1,31 @@
 def console_text_input():
+    '''
+
+    Function for user to input text
+    saved in string format through console.
+    :return:
+    string - user input information
+    User text typed in console from the function call
+    till user pressing enter.
+    '''
     pass
 
-
 def py_file_read_input():
+    '''
+     Function to input text from reading a file,
+     text is saved in string format. Only python embedded methods used.
+     :return:
+     string - file input information
+     '''
     pass
 
 
 def pandas_file_read_input():
+    '''
+       Function to input text from reading a file,
+       text is saved in string format. PANDAS methods used.
+       :return:
+       string - file input information
+       '''
     pass
 
--- app/io/output.py
+++ app/io/output.py
@@ -1,10 +1,34 @@
 def console_text_output():
+    '''
+    A function used to output string
+    text to console.
+    :return:
+    None
+    '''
     pass
 
 
-def py_file_write_output():
+def py_file_write_output(filename):
+    '''
+    A function used to output string text into a file.
+    Only Python embedded methods used.
+    Input:
+    filename - name of the file to output the text into.
+        string
+    :return:
+    None
+    '''
     pass
 
 
-def pandas_file_write_output():
+def pandas_file_write_output(filename):
+    '''
+        A function used to output string text into a file.
+        PANDAS methods used.
+        Input:
+        filename - name of the file to output the text into.
+            string
+        :return:
+        None
+        '''
     pass
