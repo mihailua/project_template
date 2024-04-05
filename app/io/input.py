ba8072361ff9d3fb405ae7a254950ac11b4b28af
Mykhailo Vakoliuk <mihaylgg@gmail.com>
2024-04-05 23:35:25 +0100

    IO function declaration

---
+++ app/io/input.py
@@ -0,0 +1,11 @@
+def console_text_input():
+    pass
+
+
+def py_file_read_input():
+    pass
+
+
+def pandas_file_read_input():
+    pass
+
---
+++ app/io/output.py
@@ -0,0 +1,10 @@
+def console_text_output():
+    pass
+
+
+def py_file_write_output():
+    pass
+
+
+def pandas_file_write_output():
+    pass
